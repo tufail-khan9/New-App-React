d4ce8f37c2395da786db03f9a4e26f5269c8c2a1
tufail-khan9 <tufailrose999@gmail.com>
2024-06-05 12:36:12 +0500

    feat:cmnt

--- profile/Projects/1-todo-app-version-one/src/App.jsx
+++ profile/Projects/1-todo-app-version-one/src/App.jsx
@@ -1,23 +1,23 @@
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
-//import MapMethod from "./components/MapMethod";
-import TodoItems from "./components/TodoItems";
+
 
 import './App.css';
 
 function App() {
-  const todoItems22 =[{name:'By Milk', dueDate: '10-2-2024'},{name: 'Go To College', dueDate:'10-2-2024'}]
+  const todoItems22 = [
+    { name: 'Buy Milk', dueDate: '10-2-2024' },
+    { name: 'Go To College', dueDate: '10-2-2024' }
+  ];
+
   return (
     <>
       <center className="todo-container">
         <AppName />
         <AddTodo />
-        <TodoItems todoItems={todoItems22}/>
-        
-        
+        <TodoItems todoItems={todoItems22} />
       </center>
       {/* <MapMethod /> */}
-     
     </>
   );
 }
--- profile/Projects/1-todo-app-version-one/src/components/TodoItems.jsx
+++
@@ -1,9 +0,0 @@
-
-export default TodoItems({todoItems}){
-    return(
-        <>
-        <TodoItem todoName="By Milk" todoDate="10-3-2024"></TodoItem>
-        <TodoItem todoName="Go To College" todoDate="10-3-2024"></TodoItem>
-        </>
-    )
-}
